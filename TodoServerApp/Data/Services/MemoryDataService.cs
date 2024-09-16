fa0eb396fa84dacb420e8e78e2b5531eaef1c882
Tyuiu.AramaAG@outlook.com <Tyuiu.AramaAG@outlook.com>
2024-09-16 22:44:50 +0500

    lecture 3

---
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -0,0 +1,20 @@
+﻿using TodoServerApp.Data.Interfaces;
+
+namespace TodoServerApp.Data.Services
+{
+    public class MemoryDataService : IDataService
+    {
+        public static IEnumerable<TaskItem> tasks { get; } = [
+            new(){ ID = 1, Title="Задание 1", Description="Описание задачи 1", CratedDate=DateTime.Now },
+            new (){ ID = 2, Title="Задание 2", Description="Описание задачи 2", CratedDate=DateTime.Now },
+            new (){ ID = 3, Title="Задание 3", Description="Описание задачи 3", CratedDate=DateTime.Now },
+        ];
+
+
+        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+        {
+            await Task.Delay(1000);
+            return await Task.FromResult(tasks);
+        }
+    }
+}
