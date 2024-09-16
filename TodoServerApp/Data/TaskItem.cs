4299ad4f5b18c079241edfeb270ae06702256cd2
Tyuiu.AramaAG@outlook.com <Tyuiu.AramaAG@outlook.com>
2024-09-16 22:06:14 +0500

    lecture2

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+        public DateTime? CratedDate { get; set; }
+        public DateTime? FInishdDate { get; set; }
+
+    }
+}
